83f1e50cb6e149b295f8f9f878506123fcae35e2
Reyam <engineer_reyam@yahoo.com>
2019-03-02 16:28:24 +0000

    completed exercise h-numbers

--- week-1/H-numbers/exercise.js
+++ week-1/H-numbers/exercise.js
@@ -1,1 +1,8 @@
 // Start by creating a variables `numberOfStudents` and `numberOfMentors`
+
+var numberOfStudents = 15;
+
+var numberOfMentors = 8;
+var sumofAll = numberOfStudents + numberOfMentors;
+
+console.log("Total number of students and mentors: " + sumofAll);
