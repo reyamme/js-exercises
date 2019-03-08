24e65a88d52a374825fe1a4d3e48ce9160a3714e
Reyam <engineer_reyam@yahoo.com>
2019-03-08 16:22:58 +0000

    complete I exercise

--- week-1/I-floats/exercise.js
+++ week-1/I-floats/exercise.js
@@ -1,2 +1,13 @@
 var numberOfStudents = 15;
 var numberOfMentors = 8;
+
+PerStu = (numberOfStudents / 23) * 100;
+PerMent = (numberOfMentors / 23) * 100;
+
+var percentStu = Math.round(PerStu);
+
+var percentMent = Math.round(PerMent);
+
+console.log("Total number of students: " + percentStu + "%");
+
+console.log("Total number of Mentors: " + percentMent + "%");
