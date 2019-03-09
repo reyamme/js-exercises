f3ade087fcb37d6701e72d41b074ec8868011bd9
Reyam <engineer_reyam@yahoo.com>
2019-03-09 12:25:02 +0000

    complete exercise c

--- week-2/C-comparison-operators/exercise.js
+++ week-2/C-comparison-operators/exercise.js
@@ -7,16 +7,16 @@
 
 var studentCount = 16;
 var mentorCount = 9;
-var moreStudentsThanMentors; // finish this statement
+var moreStudentsThanMentors = studentCount > mentorCount; // finish this statement
 
 var roomMaxCapacity = 25;
-var enoughSpaceInRoom; // finish this statement
+var enoughSpaceInRoom = studentCount + mentorCount <= roomMaxCapacity; // finish this statement
 
 var personA = "Daniel";
 var personB = "Irina";
-var sameName; // finish this statement
+var sameName = personA == personB; // finish this statement
 
-/* 
+/*
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
@@ -27,7 +27,7 @@ console.log(
 );
 console.log("Do person A and person B have the the same name?", sameName);
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   Are there more students than mentors? true
