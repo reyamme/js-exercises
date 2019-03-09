4fdee11d768126019d98989a78990fbf3d97355e
Reyam <engineer_reyam@yahoo.com>
2019-03-09 15:53:28 +0000

    complete  H

--- week-2/H-array-literals/exercise.js
+++ week-2/H-array-literals/exercise.js
@@ -4,10 +4,10 @@
   Declare some variables assigned to arrays of values
 */
 
-var numbers = []; // add numbers from 1 to 10 into this array
-var mentors; // Create an array with the names of 4 mentors
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // add numbers from 1 to 10 into this array
+var mentors = ["Daniel", "Irina", "Rares", "Nate"]; // Create an array with the names of 4 mentors
 
-/* 
+/*
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
@@ -19,7 +19,7 @@ module.exports = {
   mentors
 };
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   [1,2,3,4,5,6,7,8,9,10]
