918377ea0fb67efe2da979ba4021dcccd7fa3d43
Reyam <engineer_reyam@yahoo.com>
2019-03-09 16:19:43 +0000

    complete I

--- week-2/I-array-properties/exercise.js
+++ week-2/I-array-properties/exercise.js
@@ -6,7 +6,7 @@
 */
 
 function isEmpty(arr) {
-  return; // complete this statement
+  return arr.length === 0;
 }
 
 /*
@@ -23,7 +23,7 @@ module.exports = {
   isEmpty
 };
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   false
