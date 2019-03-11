c50d984a3f01b183a4c4a9d1cd206e5009ff9e49
Reyam <engineer_reyam@yahoo.com>
2019-03-11 18:32:30 +0000

    completed exercise 1 G

--- week-1/G-strings-methods/exercise.js
+++ week-1/G-strings-methods/exercise.js
@@ -1,6 +1,8 @@
 // Start by creating a variable `name`
 
-var name="Reyam"
-var length= name.length;
+var name = "Reyam";
+var length = name.length;
 
-console.log("My name is " +name +" and my name is " + length + " characters long");
+console.log(
+  "My name is " + name + " and my name is " + length + " characters long"
+);
