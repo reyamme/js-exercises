7ef72933d8c2c5c504c25890b09fc872c9c04c62
Reyam <engineer_reyam@yahoo.com>
2019-03-11 18:19:01 +0000

    completed exercise2 l

--- week-2/L-array-methods-2/exercise2.js
+++ week-2/L-array-methods-2/exercise2.js
@@ -1,15 +1,22 @@
 /*
   Array methods - .join()
   -------------------------
-  Complete the capitalise function 
+  Complete the capitalise function
   It should return a string with the first letter in uppercase
   For example, capitalise("hello") should return "Hello"
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalise(str) {
+  var UppC = str.charAt(0).toUpperCase() + str.slice(1);
 
-/* 
+  var arr = UppC.split(",");
+  var newStr = arr.join("-");
+
+  return newStr;
+}
+
+/*
   PLEASE IGNORE LINES BELOW
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
@@ -22,7 +29,7 @@ module.exports = {
   capitalise
 };
 
-/* 
+/*
   EXPECTED RESULT
   ---------------
   Daniel
