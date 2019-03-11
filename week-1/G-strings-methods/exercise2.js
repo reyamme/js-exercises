39162f0b9d532ab785fbdf3ea74d08b6c920c8e4
Reyam <engineer_reyam@yahoo.com>
2019-03-11 18:34:17 +0000

    completed exercise2 g

--- week-1/G-strings-methods/exercise2.js
+++ week-1/G-strings-methods/exercise2.js
@@ -1,8 +1,7 @@
 var name = "Reyam";
 var length = name.length;
-var trm= name.trim();
+var trm = name.trim();
 
 console.log(
   "My name is " + name + " and my name is " + length + " characters long"
 );
-
