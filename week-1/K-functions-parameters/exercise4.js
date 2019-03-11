5a4a06657a19777a46f7e2c6022036528605246f
Reyam <engineer_reyam@yahoo.com>
2019-03-11 18:46:21 +0000

    completed exercise4 k

--- week-1/K-functions-parameters/exercise4.js
+++ week-1/K-functions-parameters/exercise4.js
@@ -2,11 +2,10 @@
 
 // Call the function and assign to a variable `sum`
 
-function add (var1,var2){
-    return var1 + var2 ;
-
+function add(var1, var2) {
+  return var1 + var2;
 }
-var sum= add(13,124);
+var sum = add(13, 124);
 
 console.log(sum);
 
