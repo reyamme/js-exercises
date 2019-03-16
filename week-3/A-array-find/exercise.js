b935656e89e2505690e3a552888b1f3cb7000321
Reyam <engineer_reyam@yahoo.com>
2019-03-16 14:17:22 +0000

    completed

--- week-3/A-array-find/exercise.js
+++ week-3/A-array-find/exercise.js
@@ -4,8 +4,16 @@
   which starts with A and is longer than 7 letters.
 */
 
+function findWithAAnd7Letters(name) {
+  if (names.length > 7 && names.startsWith("A")) {
+    return true;
+  }
+}
+
 // -- Complete this function -->
-function findLongNameThatStartsWithA(names) {}
+function findLongNameThatStartsWithA(names) {
+  return names.find(findWithAAnd7Letters);
+}
 
 // DEBUG (check your function works by calling it with some sample input)
 var names = [
