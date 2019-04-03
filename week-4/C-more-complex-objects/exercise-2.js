30106a15db070bc4768ed6d35855e9d39f3e3d42
Reyam <engineer_reyam@yahoo.com>
2019-04-03 15:49:41 +0100

    completed c more complex objects

--- week-4/C-more-complex-objects/exercise-2.js
+++ week-4/C-more-complex-objects/exercise-2.js
@@ -1,7 +1,7 @@
 /*
     Given the same "house" object again
 
-    Follow the instructions below and then run the file 
+    Follow the instructions below and then run the file
     and make sure it outputs the correct results
 */
 
@@ -24,6 +24,9 @@ var newCurrentOwner = {
 
   WRITE YOUR CODE BELOW
 */
+house.currentOwner = newCurrentOwner;
+house.previousOwners[1] = "Stephen B.";
+house.isForSale = false;
 
 // - assign the value of the variable 'newCurrentOwner' as the value to the house's "currentOwner"
 // - from the list of previous owners, replace only "John A." with "Stephen B."
