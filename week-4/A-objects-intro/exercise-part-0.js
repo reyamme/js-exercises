52951f30baaff88a5bbaa8a892f605e20a54128c
Reyam <engineer_reyam@yahoo.com>
2019-04-03 15:39:12 +0100

    completed A objects

--- week-4/A-objects-intro/exercise-part-0.js
+++ week-4/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,9 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+
+var laptop = {
+  ScreenSize: 1200,
+  TouchScreen: false,
+  Memory: "64GB"
+};
--- week-4/A-objects-intro/exercise-part-1.js
+++ week-4/A-objects-intro/exercise-part-1.js
@@ -1,8 +1,37 @@
 /*
 
-Think of 5 different real world "things" that you 
+Think of 5 different real world "things" that you
 can describe with a JavaScript object
 
 Assign each of them to a separate variable
 
 */
+var RealWorld = {
+  Asia: "yallow",
+  Eurape: "white",
+  Austr: "it is far -_-",
+  SouthPole: "far ",
+  NorthPole: "I like it"
+};
+var country = {
+  Iraq: "yallow",
+  UK: "white",
+  Australia: "it is far -_-",
+  Syria: "far ",
+  jordan: "I like it"
+};
+
+var color = {
+  red: "no",
+  white: "sure yes",
+  purple: "my fav"
+};
+var life = {
+  travel: true,
+  luxLife: ["mercedce", "brands", "be a boss -_-"]
+};
+
+var reyam = {
+  wantToStudy: true,
+  hoppies: reading
+};
